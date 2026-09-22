ccedcdc8af8f58543503d0a40fed6df7bf4f62ab
Abraham Rubinestock <abbrubin150@gmail.com>
2026-09-22 15:14:27 +0300

    feat: add src/theme.ts

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+export const win95 = {
+  desktop: "#008080",
+  face: "#c0c0c0",
+  light: "#ffffff",
+  mid: "#808080",
+  dark: "#404040",
+  black: "#000000",
+  blue: "#000080",
+  blueText: "#ffffff",
+  field: "#ffffff",
+  fieldText: "#000000",
+  selection: "#000080",
+};
